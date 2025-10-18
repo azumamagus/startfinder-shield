b5c2f69eb8f1f6684f00132f8739f6e869f6bcbe
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2025-10-18 18:46:01 -0300

    ajustes

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Detecta se está rodando no GitHub Pages
-var baseUri = builder.HostEnvironment.BaseAddress.Contains("github.io")
-    ? "https://azumamagus.github.io/startfinder-shield/"
-    : builder.HostEnvironment.BaseAddress;
-
-// Registra o HttpClient com o endereço base correto
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUri) });
+// HttpClient usa automaticamente o BaseAddress do <base href> no index.html
+// Isso Ã© configurado pelo GitHub Actions durante o deploy
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
