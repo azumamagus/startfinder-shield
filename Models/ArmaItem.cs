b52882fc923b487211d606fca1fc0f3a2a564daf
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2025-10-18 11:51:08 -0300

    ajustando

---
+++ Models/ArmaItem.cs
@@ -0,0 +1,13 @@
+namespace Starfinder.Shield.Models;
+
+public class ArmaItem
+{
+    public string Grupo { get; set; } = string.Empty;
+    public string Nome { get; set; } = string.Empty;
+    public int Nivel { get; set; }
+    public string Preco { get; set; } = string.Empty;
+    public string Dano { get; set; } = string.Empty;
+    public string Critico { get; set; } = string.Empty;
+    public string Volume { get; set; } = string.Empty;
+    public string Especial { get; set; } = string.Empty;
+}
